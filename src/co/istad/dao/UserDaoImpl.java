495c4a6e6091dfdc29cddb8fcea3fd1e7e2079be
sunlyhuor <jkhmer1111@gmail.com>
2023-11-24 03:41:41 -0500

    modify: login

--- src/co/istad/dao/UserDaoImpl.java
+++ src/co/istad/dao/UserDaoImpl.java
@@ -3,6 +3,7 @@ package co.istad.dao;
 import co.istad.connection.ConnectionDB;
 import co.istad.model.Book;
 import co.istad.model.User;
+import co.istad.util.PasswordEncoder;
 
 import java.sql.*;
 import java.util.List;
@@ -15,17 +16,17 @@ public class UserDaoImpl implements UserDao{
     @Override
     public User create(User user) {
         String query = """
-                    INSERT INTO users (username, email, password, confirm_password, created_at, role_id)
+                    INSERT INTO users (username, email, password, created_at, role_id, salt)
                     VALUES(?, ?, ?, ?, ?, ?)
                 """;
         try(PreparedStatement statement = this.connection.prepareStatement(query)) {
-
+            byte[] salt = PasswordEncoder.generateSalt();
             statement.setString(1, user.getUsername());
             statement.setString(2, user.getEmail());
             statement.setString(3, user.getPassword());
-            statement.setString(4, user.getConfirmPassword());
-            statement.setDate(5, new Date(System.currentTimeMillis()));
-            statement.setLong(6, user.getRole().getId());
+            statement.setDate(4, new Date(System.currentTimeMillis()));
+            statement.setLong(5, user.getRole().getId());
+            statement.setBytes(6, salt);
             int affectedRow = statement.executeUpdate();
             System.out.println("Signup successfully...!");
             System.out.println(affectedRow);
@@ -39,18 +40,19 @@ public class UserDaoImpl implements UserDao{
     public User login(User user) {
         String query = """
                     SELECT * FROM users
-                    WHERE username = ? OR email = ? AND password = ?
+                    WHERE (username = ? OR email = ?) AND password = ?
                 """;
         try (PreparedStatement statement = connection.prepareStatement(query)){
             statement.setString(1, user.getUsername());
-            statement.setString(2, user.getEmail());
-            statement.setString(3, user.getPassword());
+            statement.setString(2, user.getUsername());
+            statement.setString(3, user.getPassword() );
             ResultSet rs = statement.executeQuery();
             while (rs.next()){
                 user.setEmail(rs.getString("username"));
                 System.out.println("Login successfully...!");
                 return user;
             }
+            System.out.println("Something wrong!");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
